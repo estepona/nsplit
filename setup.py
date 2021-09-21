7d4c482b66c6d79fce241c6c2831a80d77c70d4a
estepona <esteponawondering@gmail.com>
2021-09-20 22:32:52 -0400

    setup

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
 from setuptools import setup
 
-# setup(
-#   name='naive-video-splitter',
-#   version='0.0.1',
-#   packages=['src'],
-#   install_requires=[
-#     'click',
-#   ],
-# )
-
 setup()
