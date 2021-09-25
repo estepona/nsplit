2b82a4f7f805a56ec20aaa9cc62e9c64510a1ef6
estepona <esteponawondering@gmail.com>
2021-09-25 11:48:09 -0400

    equal test

--- tests/app_test.py
+++ tests/app_test.py
@@ -2,12 +2,19 @@ import os
 
 import pytest
 
-from src.app import split_, merge_
+from src.app import BYTES_PER_KB, BYTES_PER_MB, read_in_chunks, split_, merge_
 
 
+@pytest.mark.skip
 @pytest.mark.parametrize(
     'dirpath, src, dst, chunk',
     [
+        (
+            '/mnt/c/Users/estep/Videos/Captures',
+            '/mnt/c/Users/estep/Videos/Captures/dup.mov',
+            '/mnt/c/Users/estep/Videos/Captures/dup_copy.mov',
+            3,
+        ),
         (
             '/mnt/c/Users/estep/Videos/Captures',
             '/mnt/c/Users/estep/Videos/Captures/dup.mov',
@@ -16,9 +23,63 @@ from src.app import split_, merge_
         ),
         (
             '/mnt/c/Users/estep/Videos/Captures',
+            '/mnt/c/Users/estep/Videos/Captures/dup.mov',
+            '/mnt/c/Users/estep/Videos/Captures/dup_copy.mov',
+            7,
+        ),
+        (
+            '/mnt/c/Users/estep/Videos/Captures',
+            '/mnt/c/Users/estep/Videos/Captures/dup.mov',
+            '/mnt/c/Users/estep/Videos/Captures/dup_copy.mov',
+            10,
+        ),
+        (
+            '/mnt/c/Users/estep/Videos/Captures',
+            '/mnt/c/Users/estep/Videos/Captures/mgs1.mp4',
+            '/mnt/c/Users/estep/Videos/Captures/mgs1_copy.mp4',
+            3,
+        ),
+        (
+            '/mnt/c/Users/estep/Videos/Captures',
             '/mnt/c/Users/estep/Videos/Captures/mgs1.mp4',
             '/mnt/c/Users/estep/Videos/Captures/mgs1_copy.mp4',
             5,
+        ),
+        (
+            '/mnt/c/Users/estep/Videos/Captures',
+            '/mnt/c/Users/estep/Videos/Captures/mgs1.mp4',
+            '/mnt/c/Users/estep/Videos/Captures/mgs1_copy.mp4',
+            7,
+        ),
+        (
+            '/mnt/c/Users/estep/Videos/Captures',
+            '/mnt/c/Users/estep/Videos/Captures/mgs1.mp4',
+            '/mnt/c/Users/estep/Videos/Captures/mgs1_copy.mp4',
+            10,
+        ),
+        (
+            '/mnt/c/Users/estep/Videos/Captures',
+            '/mnt/c/Users/estep/Videos/Captures/medium.webm',
+            '/mnt/c/Users/estep/Videos/Captures/medium_copy.webm',
+            3,
+        ),
+        (
+            '/mnt/c/Users/estep/Videos/Captures',
+            '/mnt/c/Users/estep/Videos/Captures/medium.webm',
+            '/mnt/c/Users/estep/Videos/Captures/medium_copy.webm',
+            5,
+        ),
+        (
+            '/mnt/c/Users/estep/Videos/Captures',
+            '/mnt/c/Users/estep/Videos/Captures/medium.webm',
+            '/mnt/c/Users/estep/Videos/Captures/medium_copy.webm',
+            7,
+        ),
+        (
+            '/mnt/c/Users/estep/Videos/Captures',
+            '/mnt/c/Users/estep/Videos/Captures/medium.webm',
+            '/mnt/c/Users/estep/Videos/Captures/medium_copy.webm',
+            10,
         ),
     ],
 )
@@ -29,4 +90,112 @@ def test_size(dirpath, src, dst, chunk):
   srcfile_size = os.stat(src).st_size
   dstfile_size = os.stat(dst).st_size
 
+  os.remove(dst)
+
   assert srcfile_size == dstfile_size
+
+
+@pytest.mark.parametrize(
+    'dirpath, src, dst, chunk',
+    [
+        (
+            '/mnt/c/Users/estep/Videos/Captures',
+            '/mnt/c/Users/estep/Videos/Captures/dup.mov',
+            '/mnt/c/Users/estep/Videos/Captures/dup_copy.mov',
+            3,
+        ),
+        (
+            '/mnt/c/Users/estep/Videos/Captures',
+            '/mnt/c/Users/estep/Videos/Captures/dup.mov',
+            '/mnt/c/Users/estep/Videos/Captures/dup_copy.mov',
+            5,
+        ),
+        (
+            '/mnt/c/Users/estep/Videos/Captures',
+            '/mnt/c/Users/estep/Videos/Captures/dup.mov',
+            '/mnt/c/Users/estep/Videos/Captures/dup_copy.mov',
+            7,
+        ),
+        (
+            '/mnt/c/Users/estep/Videos/Captures',
+            '/mnt/c/Users/estep/Videos/Captures/dup.mov',
+            '/mnt/c/Users/estep/Videos/Captures/dup_copy.mov',
+            10,
+        ),
+        (
+            '/mnt/c/Users/estep/Videos/Captures',
+            '/mnt/c/Users/estep/Videos/Captures/mgs1.mp4',
+            '/mnt/c/Users/estep/Videos/Captures/mgs1_copy.mp4',
+            3,
+        ),
+        (
+            '/mnt/c/Users/estep/Videos/Captures',
+            '/mnt/c/Users/estep/Videos/Captures/mgs1.mp4',
+            '/mnt/c/Users/estep/Videos/Captures/mgs1_copy.mp4',
+            5,
+        ),
+        (
+            '/mnt/c/Users/estep/Videos/Captures',
+            '/mnt/c/Users/estep/Videos/Captures/mgs1.mp4',
+            '/mnt/c/Users/estep/Videos/Captures/mgs1_copy.mp4',
+            7,
+        ),
+        (
+            '/mnt/c/Users/estep/Videos/Captures',
+            '/mnt/c/Users/estep/Videos/Captures/mgs1.mp4',
+            '/mnt/c/Users/estep/Videos/Captures/mgs1_copy.mp4',
+            10,
+        ),
+        (
+            '/mnt/c/Users/estep/Videos/Captures',
+            '/mnt/c/Users/estep/Videos/Captures/medium.webm',
+            '/mnt/c/Users/estep/Videos/Captures/medium_copy.webm',
+            3,
+        ),
+        (
+            '/mnt/c/Users/estep/Videos/Captures',
+            '/mnt/c/Users/estep/Videos/Captures/medium.webm',
+            '/mnt/c/Users/estep/Videos/Captures/medium_copy.webm',
+            5,
+        ),
+        (
+            '/mnt/c/Users/estep/Videos/Captures',
+            '/mnt/c/Users/estep/Videos/Captures/medium.webm',
+            '/mnt/c/Users/estep/Videos/Captures/medium_copy.webm',
+            7,
+        ),
+        (
+            '/mnt/c/Users/estep/Videos/Captures',
+            '/mnt/c/Users/estep/Videos/Captures/medium.webm',
+            '/mnt/c/Users/estep/Videos/Captures/medium_copy.webm',
+            10,
+        ),
+    ],
+)
+def test_equal(dirpath, src, dst, chunk):
+  split_(src, chunk, '')
+  merge_(dirpath, True)
+
+  srcfile = open(src, 'rb')
+  dstfile = open(dst, 'rb')
+
+  src_chunk_gen = read_in_chunks(srcfile, BYTES_PER_MB)
+  dst_chunk_gen = read_in_chunks(dstfile, BYTES_PER_MB)
+
+  while True:
+    src_chunk = next(src_chunk_gen)
+    dst_chunk = next(dst_chunk_gen)
+
+    if (src_chunk is None) and (dst_chunk is None):
+      print(1)
+      break
+
+    # if (src_chunk != dst_chunk):
+    #   print(src_chunk, '\n')
+    #   print(dst_chunk)
+    #   raise Exception
+    assert src_chunk == dst_chunk
+
+  srcfile.close()
+  dstfile.close()
+  os.remove(dst)
